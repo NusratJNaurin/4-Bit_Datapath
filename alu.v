cac345507be5013b4469077c7d7d41259218d8e1
Nusrat Jahan Naurin <nn2406125@qu.edu.qa>
2026-06-05 16:12:03 +0300

    Rename 'opcode' to 'opr' in ALU module

--- alu.v
+++ alu.v
@@ -3,20 +3,20 @@
 // --- ALU MODULE ---
 // Inputs:
 //  - `A`, `B`: 4-bit operands
-//  - `opcode`: operation selector (2'b00=ADD, 2'b01=SUB, 2'b10=AND, 2'b11=OR)
+//  - `opr`: operation selector (2'b00=ADD, 2'b01=SUB, 2'b10=AND, 2'b11=OR)
 // Outputs:
 //  - `result`: 4-bit operation result
 //  - `ZF`: zero flag, high when `result` == 0
 module alu (
     input [3:0] A, 
     input [3:0] B,
-    input [1:0] opcode, //00=ADD, 01=SUB, 10=AND, 11=OR
+    input [1:0] opr, //00=ADD, 01=SUB, 10=AND, 11=OR
     output reg [3:0] result, 
     output ZF
 );
 
 always @(*) begin
-    case (opcode)
+    case (opr)
         2'b00: result = A + B;
         2'b01: result = A - B;
         2'b10: result = A & B;
